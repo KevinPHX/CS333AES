d04702e26ae533e72bba4c6ade927f882eb5e35d
Amy Weng <90800337+amycweng@users.noreply.github.com>
2023-10-21 18:40:43 -0400

    finish getting ALL structural features from Stab and Gurveych's paper

--- stab/structural.py
+++ stab/structural.py
@@ -12,6 +12,10 @@ class Structural():
         self.essay = ""
         self.paragraph_idx = [] # starting position of the first character of each paragraph (1-indexing)
         self.sentence_idx = [] # tuple with (start position of sentence, paragraph the sentence belongs to)
+        self.token_info = {}
+        self.component_info = {}
+        self.pairwise_tokens = {} 
+        self.pairwise_components = {}
 
     ''' 
     Read in relevant data: 
@@ -169,24 +173,138 @@ class Structural():
                 for info in token_dict.values(): 
                     w.writerow(info)
 
+    ''' 
+    Helper function 
+    '''
+    def paragraph_info(self): 
+        paragraph_components = {p_id: [] for p_id in range(len(self.paragraph_idx))} # p_id to components 
+        paragraph_sentences = {p_id: [] for p_id in range(len(self.paragraph_idx))} # p_id to sentence indices 
+        for sentIdx, components in self.components.items():  
+            p_id = int(self.sentence_idx[sentIdx][1])
+            paragraph_sentences[p_id].append(sentIdx)
+            for component in components: 
+                paragraph_components[p_id].append(component["name"])
+        return paragraph_components,paragraph_sentences 
+    ''' 
+    Token Statistics for Each Component 
+        For component classification:  
+        - (1) Number of tokens in component
+        - (2) Number of tokens in covering sentence 
+        - (3) Number of tokens in covering paragraph 
+        - (4) Number of tokens preceding component in sentence 
+        - (5) Number of tokens succeeding component in sentence
 
-if __name__ == "__main__": 
-    essayDir = "ArgumentAnnotatedEssays-2.0/brat-project-final"
-    annDir = "CS333AES/stab/preprocessing/src/main/resources/token_level"
-    sentDir = "CS333AES/stab/preprocessing/src/main/resources/sentence_sentiment"
+        For component stance recognition:   
+        - (2), (4), (5)
+        - (6) Ratio of number of component to sentence tokens 
+    '''
+    def token_stats(self): 
+        paragraph_tokens = {p_id: 0 for p_id in range(len(self.paragraph_idx))} # p_id to num_tokens 
+        for sentIdx in range(len(self.sentence_idx)):
+            p_id = self.sentence_idx[sentIdx][1]
+            paragraph_tokens[p_id] += len(self.annotations[sentIdx])
 
-    for file in sorted(os.listdir(essayDir)):
-        if ".ann" in file: 
-            essay_ann_file = f"{essayDir}/{file}"
-        if ".txt" in file: 
-            essay_txt_file = f"{essayDir}/{file}"
-            token_file = f"{annDir}/{file}"
-            sentence_file = f"{sentDir}/{file}"
-        # testing only with essay001.txt 
-        if "001.txt" in file: break
+        for sentIdx, components in self.components.items(): 
+            p_id = self.sentence_idx[sentIdx][1]
+            for component in components: 
+                within,preceding,following = 0,0,0
+                start = component["start"]
+                name = component["name"]
+                self.token_info[name] = {}
+                self.token_info[name]["sentence"] = len(self.annotations[sentIdx])
+                self.token_info[name]["paragraph"] = paragraph_tokens[p_id]
+                for t_dict in self.annotations[sentIdx].values(): 
+                    if t_dict["IOB"] == "O" and t_dict["start"] < start: 
+                        preceding += 1 
+                    elif t_dict["IOB"] == "O": 
+                        following += 1 
+                    else: 
+                        within += 1 
+                self.token_info[name]["within"] = within 
+                self.token_info[name]["preceding"] = preceding
+                self.token_info[name]["following"] = following                 
+
+    '''
+    Component Statistics
+    - (1) If first or last in paragraph 
+    - (2) Present in intro or conclusion
+    - (3) Relative position in paragraph 
+    - (4) Number of preceding and following components in paragraph 
+    '''
+    def component_stats(self): 
+        paragraph_components,paragraph_sentences = self.paragraph_info()
         
-    essay001 = Structural()
-    essay001.read_data(essay_ann_file, essay_txt_file, token_file, sentence_file)
-    essay001.annotate_tokens()
-    outputdir = "CS333AES/stab/token_annotations" 
-    essay001.write_data(f"{outputdir}/essay001.csv")
+        for sentIdx, components in self.components.items(): 
+            p_id = self.sentence_idx[sentIdx][1]
+            first_sent = paragraph_sentences[p_id][0]
+            last_sent = paragraph_sentences[p_id][len(paragraph_sentences[p_id])-1]
+            first_sent_start = self.annotations[first_sent][1]["start"] # first token 
+            first_sent_end = self.annotations[first_sent][len(self.annotations[first_sent])]["start"] # last token 
+            last_sent_start = self.annotations[last_sent][1]["start"]
+            last_sent_end = self.annotations[last_sent][len(self.annotations[last_sent])]["start"]
+
+            for component in components: 
+                start = component["start"]
+                name = component["name"]
+                self.component_info[name] = {"sentIdx": sentIdx, "first/last":False, "intro/conc":False, 
+                                        "num_paragraph": len(paragraph_components[p_id]),
+                                        "num_preceding": 0, "num_following": 0}
+                if p_id == 0 or p_id == len(paragraph_components)-1: 
+                    # covering paragraph is either the intro or conclusion 
+                    self.component_info[name]["intro/conc"] = True
+                if first_sent_start <= start <= first_sent_end or last_sent_start <= start <= last_sent_end: 
+                    # covering paragraph is either part of the first or last sentence in paragraph
+                    self.component_info[name]["first/last"] = True
+                for sentence in paragraph_sentences[p_id]:
+                    for component in self.components[sentence]:
+                        if component["start"] <  start: 
+                            self.component_info[name]["num_preceding"] += 1 
+                        elif component["start"] == start: 
+                            continue
+                        else: 
+                            self.component_info[name]["num_following"] += 1 
+    '''
+    Structural Features for Relation Identification
+    - (5) Number of components between source and target
+    - (6) Number of components in covering paragraph
+    - (7) If source and target are present in the same sentence
+    - (8) If target present before source 
+    - (9) If source and target are first or last component in paragraph 
+    - (10) If source and target present in introduction or conclusion
+    - Number of tokens in source 
+    - Number of tokens in target 
+    '''
+    def pairs(self): 
+        self.pairwise_tokens = {p_id: {} for p_id in range(len(self.paragraph_idx))}
+        self.pairwise_components = {p_id: {} for p_id in range(len(self.paragraph_idx))}
+        paragraph_components = self.paragraph_info()[0]
+        
+        for sentIdx, components in self.components.items(): 
+            p_id = self.sentence_idx[sentIdx][1]
+            for c1 in components:
+                c1name = c1["name"]
+                self.pairwise_tokens[p_id][c1name] = {}
+                self.pairwise_components[p_id][c1name] = {}
+                for c2name in paragraph_components[p_id]: 
+                    if c1name == c2name: continue 
+                    self.pairwise_tokens[p_id][c1name][c2name] = (self.token_info[c1name]["within"], self.token_info[c2name]["within"])
+                    
+                    if c2name in self.pairwise_components[p_id]: # already encountered this pair before 
+                        if c1name in self.pairwise_components[p_id][c2name]: 
+                            self.pairwise_components[p_id][c1name][c2name] = self.pairwise_components[p_id][c2name][c1name].copy()
+                    
+                    # novel pair 
+                    c2sentIdx = self.component_info[c2name]["sentIdx"]
+                    self.pairwise_components[p_id][c1name][c2name] = {"first/last": False, "num_between": None,
+                                                                      "num_paragraph": self.component_info[c1name]["num_paragraph"],
+                                                                      "intro/conc": self.component_info[c1name]["intro/conc"],
+                                                                      "targetBeforeSource": False, "sameSentence":sentIdx == c2sentIdx}
+                    if self.component_info[c1name]["first/last"] and self.component_info[c2name]["first/last"]: 
+                        self.pairwise_components[p_id][c1name][c2name]["first/last"] = True
+                    c1idx,c2idx = self.component_info[c1name]["num_preceding"],self.component_info[c2name]["num_preceding"]
+                    if c1idx < c2idx: 
+                        self.pairwise_components[p_id][c1name][c2name]["num_between"] = c2idx - c1idx - 1
+                        self.pairwise_components[p_id][c1name][c2name]["targetBeforeSource"] = True
+                    else: 
+                        self.pairwise_components[p_id][c1name][c2name]["num_between"] = c1idx - c2idx - 1
+
